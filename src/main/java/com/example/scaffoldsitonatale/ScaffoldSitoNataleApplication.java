e6bdee05472445cb169be435965b098d30efda43
NataleScalzo1 <n.scalzo@codermine.com>
2025-02-22 11:02:17 +0100

    first commit

---
+++ src/main/java/com/example/scaffoldsitonatale/ScaffoldSitoNataleApplication.java
@@ -0,0 +1,12 @@
+package com.example.scaffoldsitonatale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldSitoNataleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldSitoNataleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scaffoldsitonatale/ScaffoldSitoNataleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scaffoldsitonatale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldSitoNataleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
