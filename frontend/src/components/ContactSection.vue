fee703fc3aa1091ec443b2a8289a0c89247b251f
NataleScalzo1 <n.scalzo@codermine.com>
2025-02-24 12:40:42 +0100

    fix form

--- frontend/src/components/ContactSection.vue
+++ frontend/src/components/ContactSection.vue
@@ -3,11 +3,8 @@
         <h2 class="text-4xl font-bold text-white text-left mb-4 px-4 xl:pl-16">Let's Connect</h2>
         <div class="grid md:grid-cols-2 gap-4 relative px-4 xl:px-16 mt-8" data-aos="zoom-in-up">
             <div>
-                <p class="text-[#adb7be]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta alias
-                    quibusdam officia at,
-                    a quis cumque ea aperiam omnis minima quam. Modi necessitatibus odit corporis at dolorem quae iusto
-                    aliquam?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, porro!
+                <p class="text-[#adb7be]">I would love to hear from you! Whether you have a question or want to collaborate, feel free to reach out using any of the contact methods below. I am always ready to connect and discuss new opportunities or ideas.
+
                 </p>
                 <div class="col-lg-4 col-md-4 mb-lg-0 mt-5">
                     <div class="flex mb-10 items-center">
@@ -16,10 +13,13 @@
                         overflow: hidden;border: 1px solid#111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
                             <img src="https://img.icons8.com/metro/50/ffffff/new-post.png" alt="new-post" class="w-6">
                         </div>
-                        <div class="ml-5 text-white">
-                            <h4>Email</h4>
-                            <p>xxxxxxx@gmail.com</p>
-                        </div>
+                      <div class="ml-5 text-white">
+                        <h4>Email</h4>
+                        <a href="mailto:natale.scalzo.services@gmail.com">
+                          <p>natale.scalzo.services@gmail.com</p>
+                        </a>
+                      </div>
+
                     </div>
                     <div class="flex mb-10 items-center">
                         <div class="p-2"
@@ -27,10 +27,12 @@
                         overflow: hidden;border: 1px solid#111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
                             <img src="https://img.icons8.com/ios-filled/50/ffffff/phone.png" alt="phone" class="w-6">
                         </div>
-                        <div class="ml-5 text-white">
-                            <h4>Phone</h4>
-                            <p>+xxxxxxxxxx</p>
-                        </div>
+                      <div class="ml-5 text-white">
+                        <h4>Phone</h4>
+                        <a href="tel:+393895379467">
+                          <p>+39 3895379467</p>
+                        </a>
+                      </div>
                     </div>
                     <div class="flex mb-10 items-center">
                         <div class="p-2"
@@ -51,7 +53,7 @@
             overflow: hidden;border: 1px solid #111a3e;backdrop-filter: blur(9px);-webkit-backdrop-filter: blur(9px);">
             <form class="flex flex-col p-2" data-aos="zoom-in-up" >
                 <div class="mb-6">
-                    <label for="email" class="text-white block mb-2 text-sm font-medium">email</label>
+                    <label for="email" class="text-white block mb-2 text-sm font-medium">Email</label>
                     <input type="email" id="email" class="bg-[#111827] placeholder:[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                     placeholder="email@gmail.com" name="email">
                 </div>
